3681b6b3aa54f4c1e93db791dd5a642d5ff0424d
Adam Detrick <adamd@meetup.com>
2012-07-06 11:29:01 -0400

    front end polish and trying to add storage for candies

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 import yaml
+import shelve
 from random import choice
 from flask import Flask
 from flask import request 
@@ -6,23 +7,45 @@ from flask import render_template
 from flask import jsonify
 
 # starting with a simple app - hard code all photo ids
-f = open('../data.yaml')
+f = open('../urls.yaml')
 data = yaml.load(f)
 f.close()
+PhotoUrls = data['photoUrls']
 
-PhotoUrls = data['album_ids']
-
+# app settings
 app = Flask(__name__)
-
 app.debug = True
+
+# ------------------------- routes
 
 @app.route('/')
 def index():
     return render_template('index.html', photourl=choice(PhotoUrls)) 
 
+
 @app.route('/_get_new_photo')
 def get_new_photo():
-    return jsonify(id=str(choice(PhotoUrls)))
+    return jsonify(url=str(choice(PhotoUrls)))
+
+
+@app.route('/_record_candy', methods=['POST'])
+def record_candy():
+    imgurl = request.form.get('imgurl')
+    print "got"+str(imgurl)
+
+    D = shelve.open('../storage/candy', 'n')
+
+    if data and imgurl:
+        D[str(imgurl)] += 1
+        print data
+        return jsonify(success="updated candy count to "+str(data))
+    elif imgurl:
+        D[str(imgurl)] = 1
+        print data
+        return jsonify(success="recorded first candy")
+    else:
+        print "why u no url?"
+
 
 @app.route('/photo/<int:photo_id>')
 def deeplink(photo_id):
--- web/static/dogoween.js
+++ web/static/dogoween.js
@@ -3,16 +3,41 @@ $(function() {
     var D = (function($) {
         var $newDog = null,
             $candy = null,
+            $dogFrame = null,
+            $dogImg = null,
             $treat = null;
 
-        var getNewDog = function(e) {
+    var getNewDog = function(e) {
             e.preventDefault();
-            console.warn('getting new dog');
+            
+            $dogFrame.addClass('transitionable');
+            $dogFrame.addClass('loading');
+            $dogImg.hide();
+
+            $.ajax({ url: "/_get_new_photo" }).done(function(data) {
+                currentUrl = data.url;
+                $dogImg.attr('src', data.url).load(function() {
+                    $dogFrame.find('a').attr('href', data.url);
+                    $dogFrame.removeClass('transitionable');
+                    $dogFrame.removeClass('loading');
+                    $dogImg.show();
+                });
+            })
         };
 
         var giveCandy = function(e) {
             e.preventDefault();
-            console.warn('giving candy');
+
+            $.ajax({
+                type: "POST",
+                url: "/_record_candy",
+                data: {
+                    imgurl: $dogImg.attr('src')
+                },
+                success: function(data) {
+                    console.warn(data);
+                }
+            }); 
 
             $treat.show();
             $treat.addClass('transitionable');
@@ -34,6 +59,8 @@ $(function() {
 
                 //set els 
                 $newDog = $('#newdog');
+                $dogFrame = $('#dog');
+                $dogImg = $dogFrame.find('img');
                 $candy = $('#candy');
                 $treat = $('#js-treat');
 
