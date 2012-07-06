3e8a60c99686a602d88bc277c3c9acaf413befbd
Adam Detrick <adamd@meetup.com>
2012-07-05 23:43:55 -0400

    FEED DA DOG

--- web/static/dogoween.js
+++ web/static/dogoween.js
@@ -2,7 +2,8 @@ $(function() {
 
     var D = (function($) {
         var $newDog = null,
-            $candy = null;
+            $candy = null,
+            $treat = null;
 
         var getNewDog = function(e) {
             e.preventDefault();
@@ -12,11 +13,20 @@ $(function() {
         var giveCandy = function(e) {
             e.preventDefault();
             console.warn('giving candy');
+
+            $treat.show();
+            $treat.addClass('transitionable');
+            $treat.addClass('given');
         };
 
         var setupBindings = function() {
             $newDog.bind('click', getNewDog) 
             $candy.bind('click', giveCandy) 
+            
+            $('.treat').live('webkitTransitionEnd', function(e) {
+                $(this).removeClass('given');
+                $(this).removeClass('transitionable');
+            });
         };
 
         var PUBLIC = {
@@ -25,6 +35,7 @@ $(function() {
                 //set els 
                 $newDog = $('#newdog');
                 $candy = $('#candy');
+                $treat = $('#js-treat');
 
                 setupBindings();
             } 
